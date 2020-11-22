fcd60798e3e60f77714a77192dc17c002723f4bc
mraaru <70273817+mraaru@users.noreply.github.com>
2020-11-22 09:31:17 +0530

    l

--- src/Pages/Component/Container/News/index.jsx
+++ src/Pages/Component/Container/News/index.jsx
@@ -8,7 +8,8 @@ const NewsCommon = () => {
       <div className="news_box_2_start1">
         <div className="left_news_1box">
           <div className="box-101">
-        
+            <ul>
+              <li>
                 <Link to="/page">
                   <span
                     style={{
@@ -35,12 +36,388 @@ const NewsCommon = () => {
                     dignissimos, nihil maxime unde? Accusantium.
                   </p>
                 </Link>
-                         
+              </li>
+              <hr />
+              <li>
+                <Link to="/page">
+                  <img src={Bookself} alt="" />
+                </Link>
+              </li>
+            </ul>
           </div>
+          <br />
+          <hr />
+          <br />
+          <div className="box-102">
+            <ul>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur repudiandae itaque omnis commodi natus cumque?
+                    Molestiae delectus tempore dolores facere et deserunt iste
+                    nobis at reiciendis! Quidem tempore harum modi expedita
+                    laborum repudiandae vel sed corporis ut. Ducimus repellendus
+                    consequuntur corporis tempora deserunt voluptatibus
+                    voluptate quasi saepe labore? Numquam, similique.
+                  </p>
+                </Link>
+              </li>
+              <br />
+              <hr />
+              <br />
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Minima placeat impedit expedita! Iure atque molestias
+                    temporibus. Voluptatibus, mollitia nesciunt sequi
+                    praesentium neque, culpa rem odit quidem, optio incidunt
+                    excepturi. Autem nam, modi officiis dolorum numquam delectus
+                    dicta eligendi ipsum dolorem? Esse at quia voluptatem atque
+                    animi modi fugit quod autem.
+                  </p>
+                </Link>
+              </li>
+              <br />
+              <hr />
+              <br />
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Minima placeat impedit expedita! Iure atque molestias
+                    temporibus. Voluptatibus, mollitia nesciunt sequi
+                    praesentium neque, culpa rem odit quidem, optio incidunt
+                    excepturi. Autem nam, modi officiis dolorum numquam delectus
+                    dicta eligendi ipsum dolorem? Esse at quia voluptatem atque
+                    animi modi fugit quod autem.
+                  </p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <br />
+          <hr />
+          <br />
+          <div className="box-103">
+            <ul>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Dicta, voluptate. Ullam voluptate, perferendis et
+                    exercitationem sequi, maxime quo ut, iure error vero ducimus
+                    aut dolorum vel! Quisquam alias repellendus doloribus
+                    corporis, ipsam deserunt aut! Quisquam, sequi, veritatis
+                    fuga suscipit ipsam quae nam quo architecto a animi et.
+                    Voluptatem magnam officiis ad inventore? Perspiciatis
+                    ratione quia iste perferendis ad saepe quasi rerum. Tempora
+                    optio commodi eum atque dignissimos tempore magni ex.
+                  </p>
+                </Link>
+              </li>
+              <br />
+              <li>
+                <img src={Bookself} alt="" />
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Animi debitis, facere iste repudiandae veritatis
+                  necessitatibus in?
+                </p>
+              </li>
+            </ul>
+          </div>
+          <br />
+          <hr />
+          <br />
+          <div className="box-104">
+            <ul>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Praesentium ipsa reprehenderit tempora quam perferendis
+                    omnis. Molestiae in quidem nulla sequi a similique magni,
+                    vero nisi at eius eos adipisci laboriosam beatae porro
+                    numquam ipsam, eum, doloribus officiis? Optio ipsum incidunt
+                    veniam vel praesentium magnam adipisci facere accusamus,
+                    odit quos ducimus, repudiandae, rem reprehenderit? Nisi
+                    iusto adipisci, blanditiis quia tempore illo?
+                  </p>
+                </Link>
+              </li>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Praesentium ipsa reprehenderit tempora quam perferendis
+                    omnis. Molestiae in quidem nulla sequi a similique magni,
+                    vero nisi at eius eos adipisci laboriosam beatae porro
+                    numquam ipsam, eum, doloribus officiis? Optio ipsum incidunt
+                    veniam vel praesentium magnam adipisci facere accusamus,
+                    odit quos ducimus, repudiandae, rem reprehenderit? Nisi
+                    iusto adipisci, blanditiis quia tempore illo?
+                  </p>
+                </Link>
+              </li>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Praesentium ipsa reprehenderit tempora quam perferendis
+                    omnis. Molestiae in quidem nulla sequi a similique magni,
+                    vero nisi at eius eos adipisci laboriosam beatae porro
+                    numquam ipsam, eum, doloribus officiis? Optio ipsum incidunt
+                    veniam vel praesentium magnam adipisci facere accusamus,
+                    odit quos ducimus, repudiandae, rem reprehenderit? Nisi
+                    iusto adipisci, blanditiis quia tempore illo?
+                  </p>
+                </Link>
+              </li>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Praesentium ipsa reprehenderit tempora quam perferendis
+                    omnis. Molestiae in quidem nulla sequi a similique magni,
+                    vero nisi at eius eos adipisci laboriosam beatae porro
+                    numquam ipsam, eum, doloribus officiis? Optio ipsum incidunt
+                    veniam vel praesentium magnam adipisci facere accusamus,
+                    odit quos ducimus, repudiandae, rem reprehenderit? Nisi
+                    iusto adipisci, blanditiis quia tempore illo?
+                  </p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <br />
+          <hr />
+          <br />
+          <div className="box-105">
+            <ul>
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Quis distinctio ipsam tenetur inventore. Repellendus aperiam
+                    consequuntur, optio iste, nesciunt vitae dolore aspernatur
+                    minima quo ut ad? Repellat incidunt, ex facilis eius saepe,
+                    minus voluptates qui, laudantium laborum sapiente doloribus
+                    in explicabo nobis eum! Dolor exercitationem sed iure
+                    voluptas. Excepturi, facere!
+                  </p>
+                </Link>
+              </li>
+              <br />
+              <hr />
+              <br />
+              <li>
+                <Link to="/page">
+                  <span
+                    style={{
+                      fontSize: "1.4rem",
+                      fontWeight: "600",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Commodi quasi iusto eligendi dignissimos, non eveniet
+                    necessitatibus hic numquam. Excepturi, facere, deleniti
+                    explicabo officiis amet in laudantium maxime veniam animi,
+                    officia iure reprehenderit quod. Reiciendis necessitatibus
+                    perferendis repudiandae culpa quam voluptatibus natus quos
+                    tenetur dicta id ducimus, tempora ad eius asperiores.
+                  </p>
+                </Link>
+              </li>
+            </ul>
+            <hr />
+            <ul>
+              <li className="animate-101"></li>
+            </ul>
+          </div>
+          <br />
+          <hr />
+          <br />
+        </div>
+        <hr />
+        <div className="right_news_1box">
+          <Link to="/page">
+            <span
+              style={{
+                fontSize: "1.4rem",
+                fontWeight: "600",
+              }}
+            >
+              Opinion
+            </span>
+          </Link>
+          <ul>
+            <li>
+              <Link to="/page">
+                <img src={Bookself} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/page">
+                <span
+                  style={{
+                    fontSize: "1.4rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </span>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Accusantium tempore, repellat placeat, porro soluta laudantium
+                  asperiores, perspiciatis libero molestias praesentium sed
+                  corporis optio quo omnis dolor consequuntur dolores voluptatem
+                  ad veniam sit qui nesciunt autem atque similique? Eum, natus
+                  perspiciatis.
+                </p>
+              </Link>
+            </li>
+          </ul>
+          <br />
+          <hr />
+          <br />
+          <ul>
+            <li>
+              <Link to="/page">
+                <img src={Bookself} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/page">
+                <span
+                  style={{
+                    fontSize: "1.4rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </span>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Accusantium tempore, repellat placeat, porro soluta laudantium
+                  asperiores, perspiciatis libero molestias praesentium sed
+                  corporis optio quo omnis dolor consequuntur dolores voluptatem
+                  ad veniam sit qui nesciunt autem atque similique? Eum, natus
+                  perspiciatis.
+                </p>
+              </Link>
+            </li>
+          </ul>
+          <br />
+          <hr />
+          <br />
+          <ul>
+            <li>
+              <Link to="/page">
+                <img src={Bookself} alt="" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/page">
+                <span
+                  style={{
+                    fontSize: "1.4rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </span>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Accusantium tempore, repellat placeat, porro soluta laudantium
+                  asperiores, perspiciatis libero molestias praesentium sed
+                  corporis optio quo omnis dolor consequuntur dolores voluptatem
+                  ad veniam sit qui nesciunt autem atque similique? Eum, natus
+                  perspiciatis.
+                </p>
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </>
   );
 };
 
-export default NewsCommon
+export default NewsCommon;
